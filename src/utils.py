b5700b9e3e3ca1f407de2578a7c40edfacaaa000
Ishan Kapoor <ikapoor@uwaterloo.ca>
2019-10-25 00:38:59 -0700

    added src director and tests directory

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from src.constants import (
+	teams,
+	seasons
+)
+
+
+def get_season(start_year):
+	if not start_year in seasons.keys():
+		print("data not available")
+		return None
+	return pd.read_csv('data/{}'.format(seasons[start_year]))
+
+
+def get_team_season(team, start_year):
+	if team not in teams:
+		print("Invalid Team name")
+		return None
+	if start_year not in seasons.keys():
+		print("data not available")
+		return None
+	return pd.read_csv('data/{}_{}.csv'.format(team, start_year))
+L
